752a93a0ed0ad59ca2caa99979a56454642931d5
Ketki Morabkar <ketkimorabkar@ketkis-mbp.mynetworksettings.com>
2023-10-07 00:42:29 -0400

    Project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import Layout from "../components/layouts"
+import {ToastContainer} from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <ToastContainer Limit={1}/>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
